22a626b82d077d900060486f5ae07ee515e482c9
Sudhanshu8252 <sudhanshuranjan9608@gmail.com>
2026-01-22 14:56:57 +0530

    Day1 of Learing react

---
+++ DAY1/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <>
+      <div id='parent'>
+        <h1 id ='child1'>Hello World!</h1>
+        <h3 id ='child2'> Sudhanshu</h3>
+      </div>
+      <div id='sibling'>
+        This is Sibling Div
+      </div>
+    </>
+  )
+}
+
+export default App
