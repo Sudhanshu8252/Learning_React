1ab3d51f0114c831b581c332e477920265506a6b
Sudhanshu8252 <sudhanshuranjan9608@gmail.com>
2026-01-25 11:46:59 +0530

    Compled Day2

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className="card">
+    <h1>Sudhanshu Ranjan</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, illum.</p>
+  </div>
+    </div>
+  )
+}
+
+export default Card
