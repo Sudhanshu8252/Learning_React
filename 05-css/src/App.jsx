81c94c044a08802fc62cbfd0c2d3cb44c3ebcabd
Sudhanshu8252 <sudhanshuranjan9608@gmail.com>
2026-01-30 14:07:10 +0530

    Day 5 completed

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Button from './components/Button/Button'
+
+const App = () => {
+  return (
+    <div>
+      <Button/>
+      <Header/>
+
+    </div>
+  )
+}
+
+export default App
