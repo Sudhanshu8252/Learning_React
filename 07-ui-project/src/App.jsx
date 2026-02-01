4a3e50e63d86131aa3b0e08da3937f17bda22f6c
Sudhanshu8252 <sudhanshuranjan9608@gmail.com>
2026-02-02 00:15:05 +0530

    day 6 completed

---
+++ 07-ui-project/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
+
+
+const App = () => {
+
+  const users= [
+    {
+      img: 'https://plus.unsplash.com/premium_photo-1661630839991-b898e1a0ee76?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d29ya2luZyUyMHByb2Zlc3Npb25hbCUyMGdpcmx8ZW58MHx8MHx8fDA%3D',
+      intro :'',
+      color :'blue',
+      tag:'Satisfied'
+    },
+    {
+      img: 'https://plus.unsplash.com/premium_photo-1661505218403-c684557a824d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29ycG9yYXRlJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D',
+      intro :'',
+      color :'lightseagreen',
+      tag:'Underserved'
+    },
+    {
+      img: 'https://plus.unsplash.com/premium_photo-1674055047771-a7658f3f9c23?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29ycG9yYXRlJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D',
+      intro :'',
+      color :'amber',
+      tag:'Underbanked'
+    },{
+      img:'https://images.unsplash.com/photo-1748666948369-d8eb966959c8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvcnBvcmF0ZSUyMGdpcmx8ZW58MHx8MHx8fDA%3D',
+      intro:'',
+      color :'pink',
+      tag:'Average'
+    }
+  ]
+  return (
+    <div>
+      <Section1 users={users}/>
+      <Section2/>
+      </div>
+  )
+}
+
+export default App
---
+++ 07-ui-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    
+        <div className='p-6'>
+        <h3 className=' mb-7 text-7xl font-bold '> Prospective <br/> <span >customer</span> <br/>Segmentation</h3>
+        <p className ='text-xlw-[90%] font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat totam fugit voluptas exercitationem vitae optio nemo molestiae, quis aspernatur omnis!</p>
+      </div>
+  )
+}
+
+export default HeroText
