1583a5ef222ef80ba4e6b8e3860821a2acb9cf2b
Sudhanshu8252 <sudhanshuranjan9608@gmail.com>
2026-02-02 11:23:22 +0530

    updated color

--- 07-ui-project/src/App.jsx
+++ 07-ui-project/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     {
       img: 'https://plus.unsplash.com/premium_photo-1674055047771-a7658f3f9c23?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29ycG9yYXRlJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D',
       intro :'',
-      color :'amber',
+      color :'purple',
       tag:'Underbanked'
     },{
       img:'https://images.unsplash.com/photo-1748666948369-d8eb966959c8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvcnBvcmF0ZSUyMGdpcmx8ZW58MHx8MHx8fDA%3D',
